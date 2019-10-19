0ddd75cb92391d8d6a61715dc184431ae5c4c095
wss <1030462238@qq.com>
2019-10-19 11:02:38 +0800

    修改

--- unicorn_activity/src/main/java/com/unicorn/activity/ActivityApplication.java
+++ unicorn_activity/src/main/java/com/unicorn/activity/ActivityApplication.java
@@ -1,27 +1,20 @@
 package com.unicorn.activity;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.*;
+
 import util.IdUtil;
 
 /**
  * @Author:wangsusheng
  * @Date: 2019/10/17 18:01
  */
+
+
 @SpringBootApplication
 @EnableCaching
 public class ActivityApplication {
@@ -34,25 +27,5 @@ public class ActivityApplication {
         return new IdUtil(1, 1);
     }
 
-    @Configuration
-    public class Config  extends CachingConfigurerSupport {
-
-
-        @Bean
-        public CacheManager cacheManager(RedisConnectionFactory factory) {
-            RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-            Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-
-            // 配置序列化
-            RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
-            RedisCacheConfiguration redisCacheConfiguration = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
-                    .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
-
-            RedisCacheManager cacheManager = RedisCacheManager.builder(factory)
-                    .cacheDefaults(redisCacheConfiguration)
-                    .build();
-            return cacheManager;
-        }
-    }
 
 }
