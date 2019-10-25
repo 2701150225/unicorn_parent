ede5bfe10a1854d2dcbc511e0fe6b0c74c6f66b1
2701150225 <1030462238@qq.com>
2019-10-25 14:31:47 +0800

    新增Eureka服务端

---
+++ unicorn_eureka/src/main/java/com/unicorn/eureka/EurekaServer.java
@@ -0,0 +1,17 @@
+package com.unicorn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author:wangsusheng
+ * @Date: 2019/10/25 14:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class);
+    }
+}
