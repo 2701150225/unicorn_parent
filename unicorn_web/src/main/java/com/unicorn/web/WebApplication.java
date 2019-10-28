0cd7c2f2a156afd3eddc464456201ff6e24b7c64
2701150225 <1030462238@qq.com>
2019-10-28 11:28:27 +0800

    配置前台微服务网关

---
+++ unicorn_web/src/main/java/com/unicorn/web/WebApplication.java
@@ -0,0 +1,20 @@
+package com.unicorn.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * @Author:wangsusheng
+ * @Date: 2019/10/28 9:26
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
